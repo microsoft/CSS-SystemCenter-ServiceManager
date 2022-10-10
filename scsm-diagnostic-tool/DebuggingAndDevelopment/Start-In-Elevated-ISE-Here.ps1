824b05aded1717305606c38e5e1acdaf05d8662a
Kubilay Hüsmenoğlu <khusmeno@khusmeno.com>
2022-10-10 14:53:52 +0300

    bug fixes

--- scsm-diagnostic-tool/DebuggingAndDevelopment/Start-In-Elevated-ISE-Here.ps1
+++ scsm-diagnostic-tool/DebuggingAndDevelopment/Start-In-Elevated-ISE-Here.ps1
@@ -1,9 +1,9 @@
 ﻿# Debug/Develop in a location other than the tool directory stucture. That's not to clutter within the development folder.
 # "LastBuild" must be an existing sibling folder of this script's folder !!!
 # Note: LastBuild is included in  .gitignore
-$testFolder = (Join-Path (Split-Path $PSScriptRoot -Parent) -ChildPath LastBuild) # 'C:\temp\tests'
+$testFolder = (Join-Path (Split-Path $PSScriptRoot -Parent) -ChildPath LastBuild) 
 
-# version needs to be fetched from there, "before" copying the starting script to $testFolder
+# version needs to be fetched from the parent folder, "before" copying the starting script to $testFolder
 $toolVersion = Get-Content -Path (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath version.txt)
 
 [string]$scriptFilePath = Join-Path $testFolder (Split-Path $PSCommandPath -Leaf)
