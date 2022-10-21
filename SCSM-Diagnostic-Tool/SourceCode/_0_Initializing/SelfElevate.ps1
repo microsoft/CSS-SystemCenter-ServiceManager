860d1cb202d42c95ac93790ce85fca4df96f6c9b
Kubilay Hüsmenoğlu <khusmeno@khusmeno.com>
2022-10-21 14:45:41 +0300

    set executionpolicy

--- SCSM-Diagnostic-Tool/SourceCode/_0_Initializing/SelfElevate.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_0_Initializing/SelfElevate.ps1
@@ -1,7 +1,7 @@
 ﻿function SelfElevate() {
     if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
         if ([int](Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-            $CommandLine = "-File `"" + $Script:MyInvocation.MyCommand.Path + "`" " + $Script:MyInvocation.UnboundArguments
+            $CommandLine = "-ExecutionPolicy Bypass -File `"" + $Script:MyInvocation.MyCommand.Path + "`" " + $Script:MyInvocation.UnboundArguments
             Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
             Exit
         }
