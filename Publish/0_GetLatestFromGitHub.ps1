62ec8752636f0ea1a316117f04b1d194bf433eb0
Kubilay Hüsmenoğlu <khusmeno@khusmeno.com>
2022-10-22 11:00:44 +0300

    everything except publish folder

--- Publish/0_GetLatestFromGitHub.ps1
+++
@@ -1,6 +0,0 @@
-﻿# ???
-# git pull  
-
-#sample
-$File = "SCSM-Diagnostic-Tool/SourceCode/main.ps1"
-[DateTime]::Parse((git log -n 1 --format="%ad" --date=rfc $File)).ToUniversalTime().ToString("yy.MM.dd.HHmm")
--- Publish/1a_(optional)_Start_NewBuild_ToLocalDebug.ps1
+++
@@ -1,5 +0,0 @@
-Set-PSDebug -Strict # from now on, all variables must be explicitly declared before they are used  
-$Error.Clear();
-Set-Location $PSScriptRoot 
-
-. ..\Build\Start_Build_ToLocalDebug.ps1
