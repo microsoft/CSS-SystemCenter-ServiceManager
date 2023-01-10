475627f366b6e92b09a9fda1a340cb0b944559e0
name in public profle <khusmeno@hotmail.com>
2023-01-10 17:35:02 +0300

    added new rule Check_ToolOutdated() (#20)
    Co-authored-by: Kubilay <khusmeno@khusmeno.com>

---
+++ SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/Analyze_ForALL/Check_ToolOutdated.ps1
@@ -0,0 +1,22 @@
+﻿function Check_ToolOutdated() {
+
+    $toolSelfUpdateDays = 14
+
+    $dataRow = GetEmptyResultRow
+    $dataRow.RuleName="Tool up-to-date"
+    $dataRow.RuleDesc="This SCSM Diagnostic Tool is designed to update itself with new Rules. If for any reason (no internet etc.) it couldn't self-update in the last $toolSelfUpdateDays days, then there's probably a newer version at $(GetAnchorForExternal 'https://aka.ms/scsm-diagnostic-tool')."
+    $dataRow.ProblemSeverity=[ProblemSeverity]::Warning
+    
+    $scriptLastUpdatedNdaysAgo = ( (Get-Date) - ((Get-ChildItem -Path $scriptFilePath).LastWriteTime) ).Days
+    
+    if ($scriptLastUpdatedNdaysAgo -gt $toolSelfUpdateDays) {
+        $dataRow.RuleResult = "Please replace '$scriptFilePath' with the latest version from $(GetAnchorForExternal 'https://aka.ms/scsm-diagnostic-tool'). <br/>$(IgnoreRuleIfText) explicitly asked by Microsoft Support to run this specific version."
+        $Result_Problems += $dataRow
+    }
+    else {
+        $dataRow.RuleResult += "This tool was updated $scriptLastUpdatedNdaysAgo days ago."
+        $Result_OKs += $dataRow 
+    }
+   
+}
+
