81ce8a4afc578be516d5723eee41f7d72ca45a04
Kubilay <khusmeno@khusmeno.com>
2023-01-11 16:29:18 +0300

    fixed error output in Collect_ProgramsInfo()

--- SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_NonScsmSpecificInfo/Collect_ProgramsInfo.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_NonScsmSpecificInfo/Collect_ProgramsInfo.ps1
@@ -1,3 +1,18 @@
 ﻿function Collect_ProgramsInfo() {
-    Start_Async -code { Get-WmiObject -Class Win32_Product | Select-Object Version, Name, InstallDate | ft } -outputFileName "ProgramVersions.txt" 
+    Start_Async -code { $input=$null; Get-WmiObject -Class Win32_Product | Select-Object Version, Name, InstallDate | ft } -outputFileName "ProgramVersions.txt" 
+
+<#
+NOTE: 
+Do not remove
+    $input=$null;
+
+it was added, because otherwise, below error was set to -ErrorVariable later at Receive-Job.  
+
+    The input object cannot be bound to any parameters for the command either because the command does not take pipeline input or the input and its properties do not match any of the 
+parameters 
+that take pipeline input.
+    + CategoryInfo          : InvalidArgument: (:PSObject) [Get-WmiObject], ParameterBindingException
+    + FullyQualifiedErrorId : InputObjectNotBound,Microsoft.PowerShell.Commands.GetWmiObjectCommand
+    + PSComputerName        : localhost
+#>
 }
