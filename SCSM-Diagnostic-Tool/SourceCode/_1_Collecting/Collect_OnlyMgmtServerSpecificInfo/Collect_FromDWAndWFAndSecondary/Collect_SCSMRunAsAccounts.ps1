13bb5aa9a85263d68e9b43f38b784c4bfb63307a
Kubilay <khusmeno@khusmeno.com>
2023-03-21 12:31:33 +0300

    The current Get-SCSMRunAsAccount.txt contains localized run as account names. This csv will also add the column SecureReference, which is NOT localized so that it can be later consumed by Analyzer easily, if needed.

--- SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_OnlyMgmtServerSpecificInfo/Collect_FromDWAndWFAndSecondary/Collect_SCSMRunAsAccounts.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_OnlyMgmtServerSpecificInfo/Collect_FromDWAndWFAndSecondary/Collect_SCSMRunAsAccounts.ps1
@@ -1,3 +1,5 @@
 ﻿function Collect_SCSMRunAsAccounts() {
     AppendOutputToFileInTargetFolder (Get-SCSMRunAsAccount) "Get-SCSMRunAsAccount.txt"
+
+    AppendOutputToFileInTargetFolder (Get-SCSMRunAsAccount | ConvertTo-Csv) "Get-SCSMRunAsAccount.csv"
 }
