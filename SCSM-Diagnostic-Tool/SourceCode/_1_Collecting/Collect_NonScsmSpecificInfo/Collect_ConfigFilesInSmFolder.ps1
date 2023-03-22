3a94f1c61034296045aa31ce9b7592f7032a80df
Kubilay <khusmeno@khusmeno.com>
2023-03-22 15:50:42 +0300

    collecting *.config from sm folder

---
+++ SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_NonScsmSpecificInfo/Collect_ConfigFilesInSmFolder.ps1
@@ -0,0 +1,7 @@
+﻿function Collect_ConfigFilesInSmFolder() {
+# TODO: this should be excluded for PortalOnly
+    $targetFolder = "SMFolder"
+    CreateNewFolderInTargetFolder $targetFolder
+    Get-ChildItem -Path (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\System Center\2010\Service Manager\Setup').InstallDirectory -Filter *.config | Copy-Item -Destination (GetFileNameInTargetFolder $targetFolder) -ErrorAction SilentlyContinue
+    
+}
