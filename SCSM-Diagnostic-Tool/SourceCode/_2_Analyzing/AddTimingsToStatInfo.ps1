6e502702bf0bc3818747a5c36da2be4032e7dc98
Kubilay Hüsmenoğlu <99029864+khusmeno-MS@users.noreply.github.com>
2023-06-06 18:31:08 +0300

    Update AddTimingsToStatInfo.ps1

--- SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
@@ -1,4 +1,4 @@
-﻿function AddTimingsToStatInfo() {
+function AddTimingsToStatInfo() {
 
     $rawData =  GetFileContentInSourceFolder Collector-MeasuredScriptBlocks.csv | ConvertFrom-Csv
     $rawData += GetFileContentInTargetFolder Analyzer-MeasuredScriptBlocks.csv  | ConvertFrom-Csv
@@ -12,6 +12,9 @@
     }
 
     $timings = (GetStatInfo).CreateNode([System.Xml.XmlNodeType]::Element, "Timings", $null)
+    $smdtTotalSecs = ( ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunFinish") , "yyyy-MM-dd__HH.mm.ss.fff", $null) ) `
+                     - ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunStart")  , "yyyy-MM-dd__HH.mm.ss.fff", $null) )).TotalSeconds
+    $timings.SetAttribute("SmdtTotalSecs", [Math]::Truncate($smdtTotalSecs) )
     $timings.InnerText = $minData  
 
     AddToStatInfoRoot $timings
