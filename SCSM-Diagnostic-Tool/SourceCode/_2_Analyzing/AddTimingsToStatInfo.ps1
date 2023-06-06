6fd08ad349274c0a99a1314a1e098adf4aefeb28
Kubilay <khusmeno@khusmeno.com>
2023-06-06 17:43:17 +0300

    fixes for StatInfo and more info

--- SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
@@ -12,6 +12,9 @@
     }
 
     $timings = (GetStatInfo).CreateNode([System.Xml.XmlNodeType]::Element, "Timings", $null)
+    $smdtTotalSecs = ( ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunFinish") , "yyyy-MM-dd__HH.mm.ss.fff", $null) ) `
+                     - ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunStart")  , "yyyy-MM-dd__HH.mm.ss.fff", $null) )).TotalSeconds
+    $timings.SetAttribute("SmdtTotalSecs", [Math]::Truncate($smdtTotalSecs) )
     $timings.InnerText = $minData  
 
     AddToStatInfoRoot $timings
