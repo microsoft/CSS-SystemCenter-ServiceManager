10c9684a1fc1d6dfa3af88d4da6c928a4724b6aa
Kubilay <khusmeno@khusmeno.com>
2023-06-16 17:23:04 +0300

    hided msinfo32, timings now XML

--- SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_NonScsmSpecificInfo/Collect_MSINFO32.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_NonScsmSpecificInfo/Collect_MSINFO32.ps1
@@ -1,7 +1,25 @@
 ﻿function Collect_MSINFO32() {
-#Starting MSINFO32 in the background
+#Starting MSINFO32 in the background and hide it after 2 seconds
  
     Start-Job -ScriptBlock {
-        Start-Process msinfo32.exe -Wait -ArgumentList $input 
+
+        $win32 = Add-Type -Namespace Win32 -Name Funcs -PassThru -MemberDefinition @'
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+'@
+        $SW_HIDE = 0;
+
+        $p = Start-Process msinfo32.exe -PassThru -ArgumentList $input
+
+        Start-Sleep -Seconds 2
+        $hWnd = $win32::FindWindow( '#32770', "System Information")
+        if ($hWnd -ne 0) {
+            $win32::ShowWindow($hWnd, $SW_HIDE)
+        }
+        $p.WaitForExit()
+
     } -InputObject  "/report ""$((GetFileNameInTargetFolder "msinfo32.txt"))""" | Out-Null
 }
--- SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/AddTimingsToStatInfo.ps1
@@ -1,21 +1,22 @@
 function AddTimingsToStatInfo() {
 
-    $rawData =  GetFileContentInSourceFolder Collector-MeasuredScriptBlocks.csv | ConvertFrom-Csv
-    $rawData += GetFileContentInTargetFolder Analyzer-MeasuredScriptBlocks.csv  | ConvertFrom-Csv
-    $rawDataSorted = $rawData | Sort-Object -Property Duration -Descending
-
-    [string]$minData = "msec,info`n"
-    foreach($line in $rawDataSorted) {        
-        $msec = [timespan]::Parse($line.Duration).TotalMilliseconds
-        $info = $line.ScriptBlockText
-        $minData += "$msec,$info`n"
-    }
-
     $timings = (GetStatInfo).CreateNode([System.Xml.XmlNodeType]::Element, "Timings", $null)
     $smdtTotalSecs = ( ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunFinish") , "yyyy-MM-dd__HH.mm.ss.fff", $null) ) `
                      - ( [datetime]::ParseExact( (GetStatInfoRoot).GetAttribute("SmdtRunStart")  , "yyyy-MM-dd__HH.mm.ss.fff", $null) )).TotalSeconds
     $timings.SetAttribute("SmdtTotalSecs", [Math]::Truncate($smdtTotalSecs) )
-    $timings.InnerText = $minData  
 
+    $rawData =  GetFileContentInSourceFolder Collector-MeasuredScriptBlocks.csv | ConvertFrom-Csv
+    $rawData += GetFileContentInTargetFolder Analyzer-MeasuredScriptBlocks.csv  | ConvertFrom-Csv
+    $rawDataSorted = $rawData | Sort-Object -Property Duration -Descending
+
+    foreach($line in $rawDataSorted) {        
+        $msecs = [timespan]::Parse($line.Duration).TotalMilliseconds
+        $info = $line.ScriptBlockText
+       
+        $timing = CreateElementForStatInfo -elemTagName Timing
+        $timing.SetAttribute("Info", $info )
+        $timing.SetAttribute("Msecs", $msecs )
+        $timings.AppendChild( $timing )
+    }
     AddToStatInfoRoot $timings
 }
