d4f9cecc6f2a669d7c3d3136df00d96fa02cf48e
Kubilay <khusmeno@khusmeno.com>
2023-07-06 17:50:04 +0300

    Fixed 1.Generate_ContentOf_ShowTheFindings

--- SCSM-Diagnostic-Tool/Build/1.Generate_ContentOf_ShowTheFindings.ps1
+++ SCSM-Diagnostic-Tool/Build/1.Generate_ContentOf_ShowTheFindings.ps1
@@ -1,12 +1,15 @@
 ﻿Set-PSDebug -Strict # from now on, all variables must be explicitly declared before they are used  
 $Error.Clear();
 Set-Location $PSScriptRoot
+Write-Host "PWD: $PWD"
 
 $sourceStartingScriptFolderName = 'SourceCode'
 $targetBuildFolderName = 'LocalDebug'
 $parentPath = Split-Path -Path (Get-Location) -Parent
-$sourceStartingFolderPath = Join-Path -Path $parentPath -ChildPath $sourceStartingScriptFolderName 
+New-Item -ItemType Directory -Force -Path (Join-Path -Path $parentPath -ChildPath $targetBuildFolderName) | Out-Null
 
+$sourceStartingFolderPath = Join-Path -Path $parentPath -ChildPath $sourceStartingScriptFolderName 
+Write-Host "Source folder: $sourceStartingFolderPath"
 . ( Join-Path -Path $sourceStartingFolderPath -ChildPath "_1_Collecting\Helper_Functions.ps1" )
 . ( Join-Path -Path $sourceStartingFolderPath -ChildPath "_2_Analyzing\GetShowTheFindingsPS1Content.ps1" )
 
