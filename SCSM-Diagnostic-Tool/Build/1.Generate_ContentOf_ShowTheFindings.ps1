a8f4dacf7b05fd7d0396996dbf9d9006ea8d8aac
name in public profle <khusmeno@hotmail.com>
2023-07-06 17:21:36 +0300

    Integrated ShowTheFindingsPS1Content (#89)
    Co-authored-by: Kubilay <khusmeno@khusmeno.com>

---
+++ SCSM-Diagnostic-Tool/Build/1.Generate_ContentOf_ShowTheFindings.ps1
@@ -0,0 +1,18 @@
+﻿Set-PSDebug -Strict # from now on, all variables must be explicitly declared before they are used  
+$Error.Clear();
+Set-Location $PSScriptRoot
+
+$sourceStartingScriptFolderName = 'SourceCode'
+$targetBuildFolderName = 'LocalDebug'
+$parentPath = Split-Path -Path (Get-Location) -Parent
+$sourceStartingFolderPath = Join-Path -Path $parentPath -ChildPath $sourceStartingScriptFolderName 
+
+. ( Join-Path -Path $sourceStartingFolderPath -ChildPath "_1_Collecting\Helper_Functions.ps1" )
+. ( Join-Path -Path $sourceStartingFolderPath -ChildPath "_2_Analyzing\GetShowTheFindingsPS1Content.ps1" )
+
+$analyzer_FolderName   = "Analyzer"
+$findingsHtml_FileName = "Findings.html"
+$findingsTxt_FileName  = "Findings.txt"
+$findingsPS1Content    = GetShowTheFindingsPS1Content
+
+Set-Content -Path "..\$targetBuildFolderName\GetShowTheFindingsPS1Content.ps1" -Value $findingsPS1Content
