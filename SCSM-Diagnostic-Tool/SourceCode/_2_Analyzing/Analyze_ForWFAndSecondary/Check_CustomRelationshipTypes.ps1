4fb27853fa1c3d003199d29125556569a2403c97
Kubilay <khusmeno@khusmeno.com>
2024-03-19 20:13:05 +0300

    Fix severity of rule "Custom Relationship Types"

--- SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/Analyze_ForWFAndSecondary/Check_CustomRelationshipTypes.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_2_Analyzing/Analyze_ForWFAndSecondary/Check_CustomRelationshipTypes.ps1
@@ -2,16 +2,11 @@
  $dataRow = GetEmptyResultRow
     $dataRow.RuleName="Custom Relationship Types"
     $dataRow.RuleDesc=@"
-    Custom Relationship Types with MaxCardinality=1 Targets should be excluded, if they are really not needed. Otherwise, WFs can lag and Consoles can face slowness. Please correlate with 'ECL row count' rule.
+    Custom Relationship Types with MaxCardinality=1 Targets should be excluded, if they are really not needed. Otherwise, WFs can lag and Consoles can face slowness. This rule is correlated with the 'ECL row count' rule.
 <br/>More detailed info at Appendix section in this $(GetAnchorForExternal 'https://techcommunity.microsoft.com/t5/system-center-blog/troubleshooting-workflow-performance-and-delays/ba-p/347510' 'Doc.')
 <br/>Query results are in $(CollectorLink SQL_TroubleshootingWorkflowPerformanceandDelays.csv)
 "@
     $dataRow.SAPCategories = "wf*" 
-    $dataRow.RuleResult = @"
-    Non-excluded Custom Relationship Types have been found. Please correlate with 'ECL row count' rule, because ECL table can get bigger because of this issue. It has to be confirmed, if they really need to be excluded. If so, run the below SQL script to fix this issue.
-    <br/>$(IgnoreRuleIfText) currently no WF slowness exists, but it's strongly recommended to fix this before getting into issues.<br/>
-"@
-    $dataRow.ProblemSeverity=[ProblemSeverity]::Error
 
     $linesIn_SQL_TroubleshootingWorkflowPerformanceandDelays = ConvertFrom-Csv (GetSanitizedCsv ( GetFileContentInSourceFolder SQL_TroubleshootingWorkflowPerformanceandDelays.csv) ) 
     $IncorrectRowcount = 0
@@ -26,12 +21,23 @@
         $itemsToInsert = $itemsToInsert.Remove(0,1)
     }
 
+    [cData]$eclResult = GetRuleFromAllRules "ECL row count"
+    $dataRow.ProblemSeverity= $eclResult.ProblemSeverity
+
     if ($IncorrectRowcount -eq 0) {
-        $dataRow.RuleResult="No incorrect Custom Relationship Types found."
+        $dataRow.RuleResult="No problematic Custom Relationship Types found."
         $Result_OKs += $dataRow 
     }
     else {
-        $dataRow.RuleResult += "<br/>INSERT INTO ExcludedRelatedEntityChangeLog (RelationshipTypeId, TargetTypeId) VALUES<br/> $itemsToInsert"
-        $Result_Problems += $dataRow
+        $dataRow.RuleResult = "Non-excluded Custom Relationship Types have been found. "
+        if ( (RulePassed "ECL row count") ) {
+            $dataRow.RuleResult += "However, as the 'ECL row count' rule has passed, this rule will also pass for now. But it's strongly recommended to fix this, before getting into issues. "
+            $Result_OKs += $dataRow 
+        }
+        else {
+            $dataRow.RuleResult += "This can be the reason why the ECL table is so big. "
+            $Result_Problems += $dataRow
+        }
+        $dataRow.RuleResult += "Run the below SQL script to fix this issue.<br/>INSERT INTO ExcludedRelatedEntityChangeLog (RelationshipTypeId, TargetTypeId) VALUES<br/> $itemsToInsert"
     }
 }
