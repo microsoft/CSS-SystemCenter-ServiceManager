4b0044aa22bae17b66eb679be579b23f350a5093
khusmeno <khusmeno@hotmail.com>
2025-10-30 11:48:34 +0300

    Added Get-SCSMChannel_WithAuthentication.txt

--- SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_OnlyMgmtServerSpecificInfo/Collect_FromWFAndSecondary/Collect_NotificationChannel.ps1
+++ SCSM-Diagnostic-Tool/SourceCode/_1_Collecting/Collect_OnlyMgmtServerSpecificInfo/Collect_FromWFAndSecondary/Collect_NotificationChannel.ps1
@@ -1,3 +1,4 @@
 ﻿function Collect_NotificationChannel() {
     AppendOutputToFileInTargetFolder (Get-SCSMChannel | fl *) "Get-SCSMChannel.txt"
+	AppendOutputToFileInTargetFolder (Get-SCSMChannel | Select-Object -ExpandProperty ConfigurationSources | Select-Object -Property SequenceNumber, Authentication | Sort-Object -Property SequenceNumber) "Get-SCSMChannel_WithAuthentication.txt"
 }
